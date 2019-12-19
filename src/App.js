3bdde51aa36cb0383a84d0c8e04b3208063ccf6c
Matthew Lally <shinsyotta@gmail.com>
2019-12-19 11:25:24 -0500

    Remove type

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ await channel.deposit(payload)
 // import { AddressZero } from "ethers/constants";
 // import { parseEther } from "ethers/utils";
 
-const payload: TransferParams = {
+const payload = {
   recipient: "xpub6EFZf2akgmkPEy49K4xmZgwfspYRjfy89j3WUCtbk53XAeZApE7dDWJrTv7nvoQPYWEY9w5zjnR5PmGKUh9FBWJiJsSoyb6vNPKQYELr7HS"  // <-- Laptop  //counterparty's xPub // Desktop: xpub6E4pdmd9dV7vWMExS4ComHiPj7STndSmyWJWyn4G2U8sDGcxwp4V6TDkS2WScCCMtUtfFSaND69HySuZag25PG7K7PoYYVxABWtm5tZip2f
   meta: "Metadata for transfer" // any string value, or omit
   amount: parseEther("0.1").toString() // in wei (ethers.js methods are very convenient for getting wei amounts)
